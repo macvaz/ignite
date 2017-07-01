89c8ea147e5d28c98a1a7763005c71f24b5842b3
Miguel Angel Cañas Vaz <mac@tid.es>
2017-07-01 13:16:52 +0200

    Initial version

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.lang.IgniteCallable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Ignite ignite = Ignition.start("ignite.xml")) {
+            Collection<IgniteCallable<Integer>> calls = new ArrayList<>();
+
+            // Iterate through all the words in the sentence and create Callable jobs.
+            for (final String word : "Count characters using callable".split(" "))
+                calls.add(word::length);
+
+            // Execute collection of Callables on the grid.
+            Collection<Integer> res = ignite.compute().call(calls);
+
+            // Add up all the results.
+            int sum = res.stream().mapToInt(Integer::intValue).sum();
+
+            System.out.println("Total number of characters is '" + sum + "'.");
+        }
+    }
+}
